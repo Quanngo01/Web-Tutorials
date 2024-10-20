3f632dcf98dc2edfbcd3a53b719fe0a71c4f8ca9
Quan <ngoquansz2001@gmail.com>
2024-10-20 15:10:23 +0200

    add project 04

--- main.js
+++ main.js
@@ -42,3 +42,12 @@ function update() {
   next.disabled = currentActive == steps.length;
   prog.style.width = ((currentActive - 1) / (steps.length - 1)) * 100 + "%";
 }
+
+const search = document.querySelector(".search");
+const btn = document.querySelector(".search-btn");
+const input = document.querySelector(".input");
+
+btn.addEventListener("click", () => {
+  search.classList.toggle("active");
+  input.focus();
+});
